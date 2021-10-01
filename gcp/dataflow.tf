672e3a8076816614f6103019ac9841896282bfa7
jjchavanne <jjchavanne@gmail.com>
2021-09-30 23:50:46 -0400

    fixed dataflow.tf file

--- gcp/dataflow.tf
+++ gcp/dataflow.tf
@@ -20,7 +20,7 @@ variable "custom_role_flowlog_compression_permissions_project" {
 ##############################
 resource "google_project_iam_custom_role" "prisma_cloud_custom_role_flowlog_compression" {
   project     = var.project_id
-  count       = var.flowlog_compression_project != "enabled" ? 1 : 0
+  count       = var.flowlog_compression_project == "enabled" ? 1 : 0
   role_id     = "prismaCloudFlowLogCompressor${random_string.unique_id.result}"
   title       = "Prisma Cloud Flow Logs Compressor ${random_string.unique_id.result}"
   description = "This is a custom role created for Prisma Cloud. Contains granular permission which is needed for flow logs compression"
@@ -39,9 +39,10 @@ resource "google_project_iam_member" "bind_role_dataflow-admin" {
 }
 
 resource "google_project_iam_member" "bind-role-prisma-cloud-flowlog-compressor" {
-  project = var.project_id
-  role    = "projects/${var.project_id}/roles/${google_project_iam_custom_role.prisma_cloud_custom_role_flowlog_compression.role_id}"
-  member  = "serviceAccount:${google_service_account.prisma_cloud_service_account.email}"
+  project   = var.project_id
+  count     = var.flowlog_compression_project == "enabled" ? 1 : 0
+  role      = "projects/${var.project_id}/roles/${google_project_iam_custom_role.prisma_cloud_custom_role_flowlog_compression[0].role_id}"
+  member   = "serviceAccount:${google_service_account.prisma_cloud_service_account.email}"
 }
 
 ###################
